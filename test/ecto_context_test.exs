5e0896887776881a9eaad122ff9dbea1cf4d5de2
Sascha Brink <sascha.brink@gmail.com>
2019-04-07 00:03:31 +0200

    Add functions to template

--- test/ecto_context_test.exs
+++ test/ecto_context_test.exs
@@ -7,6 +7,16 @@ defmodule EctoContextTest do
   end
 
   test "generation of functions" do
-    assert EctoContextTest.Posts.__info__(:functions) == [list: 0]
+    assert EctoContextTest.Posts.__info__(:functions) == [
+      change: 1, change: 2,
+      create: 0, create: 1, create: 2,
+      delete: 1,
+      get: 1, get: 2,
+      get!: 1, get!: 2,
+      list: 0, list: 1,
+      list_for_user: 1, list_for_user: 2,
+      schema: 0, schema: 1,
+      update: 2, update: 3,
+    ]
   end
 end
