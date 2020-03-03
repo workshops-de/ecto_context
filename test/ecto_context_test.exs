0f1674e7b787e7788e602f707e8af8ad13ab97f2
Sascha Brink <sascha.brink@gmail.com>
2020-03-03 04:26:56 +0100

    Format test file

--- test/ecto_context_test.exs
+++ test/ecto_context_test.exs
@@ -8,15 +8,24 @@ defmodule EctoContextTest do
 
   test "generation of functions" do
     assert EctoContextTest.Posts.__info__(:functions) == [
-      change: 1, change: 2,
-      create: 0, create: 1, create: 2,
-      delete: 1,
-      get: 1, get: 2,
-      get!: 1, get!: 2,
-      list: 0, list: 1,
-      list_for_user: 1, list_for_user: 2,
-      schema: 0, schema: 1,
-      update: 2, update: 3,
-    ]
+             change: 1,
+             change: 2,
+             create: 0,
+             create: 1,
+             create: 2,
+             delete: 1,
+             get: 1,
+             get: 2,
+             get!: 1,
+             get!: 2,
+             list: 0,
+             list: 1,
+             list_for_user: 1,
+             list_for_user: 2,
+             schema: 0,
+             schema: 1,
+             update: 2,
+             update: 3
+           ]
   end
 end
