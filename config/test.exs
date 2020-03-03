ebe5a4cc570a690393afa32ca4a49953cafdf8dd
Sascha Brink <sascha.brink@gmail.com>
2020-03-03 18:36:13 +0100

    Set user/password for test env

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :ecto_context, ecto_repos: [EctoContext.Test.Repo]
 
 config :ecto_context, EctoContext.Test.Repo,
-  database: "ecto_context_test",
+  url: "ecto://postgres:postgres@localhost/ecto_context_test",
   pool: Ecto.Adapters.SQL.Sandbox,
   priv: "test/support/"
 
