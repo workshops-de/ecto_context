02982a1d9b653407ae4683753f388d8aa4c93f9a
Sascha Brink <sascha.brink@gmail.com>
2020-03-03 03:30:26 +0100

    Set minimum elixir version 1.10 for new formatting options

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EctoContext.MixProject do
     [
       app: :ecto_context,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
