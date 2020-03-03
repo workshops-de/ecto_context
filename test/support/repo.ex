38aa1909186123ade716cb58b92590168c2aec6a
Sascha Brink <sascha.brink@gmail.com>
2020-03-03 18:31:28 +0100

    Remove outcommented code

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -1,7 +1,3 @@
 defmodule EctoContext.Test.Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres, otp_app: :ecto_context
-
-  #def init(_type, config) do
-  #  {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
-  #end
 end
